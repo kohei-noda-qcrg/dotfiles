002734bf7f19f701602e64d87f84d649be76c572
Kohei Noda <ponserviceregist@gmail.com>
2025-01-10 23:01:34 +0900

    setup.sh: make DOTFILES list

--- setup.sh
+++ setup.sh
@@ -12,14 +12,15 @@ yes | ~/.fzf/install
 mkdir -p "$HOME/z"
 wget https://raw.githubusercontent.com/rupa/z/master/z.sh -O "$HOME/z/z.sh"
 
-DOTFILES=$(find . -maxdepth 1 -type f | grep "^\./\.")  # Find all dotfiles in the current directory
+DOTFILES=($(find . -maxdepth 1 -type f | grep "^\./\."))  # Find all dotfiles in the current directory
 
 for file in "${DOTFILES[@]}"
 do
+  echo $file
   if [ "$file" == "./.gitignore" ]; then
     continue
   fi
-  ln -s "$FILEPATH/$file" "$HOME/$file"
+  ln -sf "$FILEPATH/$file" "$HOME/$file"
 done
 
 # ~/.local/share/vim-lsp-settings/servers/pylsp-all/venv/bin/pip installl pylsp-mypy pylsp-black
