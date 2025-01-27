1f6cc9b7c12e8ad63a0607d84b30e39a744ad9c0
Kohei Noda <ponserviceregist@gmail.com>
2025-01-27 18:10:25 +0900

    setup: doesn't install fzf and z in the dotfile setup

--- setup.sh
+++ setup.sh
@@ -4,14 +4,6 @@ set -euo pipefail
 FILEPATH=$(cd "$(dirname "$0")" && pwd)
 pushd "$FILEPATH" || exit
 
-# fzf
-git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-yes | ~/.fzf/install
-
-# z
-mkdir -p "$HOME/z"
-wget https://raw.githubusercontent.com/rupa/z/master/z.sh -O "$HOME/z/z.sh"
-
 DOTFILES=($(find . -maxdepth 1 -type f | grep "^\./\."))  # Find all dotfiles in the current directory
 
 for file in "${DOTFILES[@]}"
