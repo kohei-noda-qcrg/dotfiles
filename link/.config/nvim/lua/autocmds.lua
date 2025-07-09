4c3534c0108a2175f43cfe098d629b06a84bf453
Kohei Noda <k.noda@saver.jp>
2025-07-09 15:24:28 +0900

    link: nvim: autocmds: check format support

--- link/.config/nvim/lua/autocmds.lua
+++ link/.config/nvim/lua/autocmds.lua
@@ -1,7 +1,13 @@
 -- format on save
 vim.api.nvim_create_autocmd("BufWritePre", {
-    callback = function()
-        vim.lsp.buf.format({ async = false })
+    callback = function(args)
+        local clients = vim.lsp.get_clients({ bufnr = args.buf })
+        for _, client in ipairs(clients) do
+            if client.supports_method("textDocument/formatting") then
+                vim.lsp.buf.format({ async = false })
+                return
+            end
+        end
     end,
     group = vim.api.nvim_create_augroup("LspFormatting", { clear = true }),
 })
