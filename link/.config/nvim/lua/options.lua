6a51768e2e84f1456891f2d0c0a322cc2ddb9583
Kohei Noda <k.noda@saver.jp>
2025-07-17 10:01:44 +0900

    link: nvim: options.lua: set mapleader to space

--- link/.config/nvim/lua/options.lua
+++ link/.config/nvim/lua/options.lua
@@ -1,4 +1,5 @@
 -- Basic vim.opt settings
+vim.g.mapleader = " "
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
@@ -11,4 +12,4 @@ vim.opt.wildmode = 'longest,list'
 vim.opt.cursorline = true
 vim.opt.foldenable = false
 vim.opt.updatetime = 500
-vim.opt.clipboard:append({"unnamedplus"})
+vim.opt.clipboard:append({ "unnamedplus" })
