672b4518c2e57b15a77454c1644dfcd29f82efd4
Kohei Noda <k.noda@saver.jp>
2026-04-07 10:34:02 +0900

    link: nvim: add Format{Enable,Disable} command and the formatter use this.

--- link/.config/nvim/lua/commands.lua
+++ link/.config/nvim/lua/commands.lua
@@ -2,3 +2,24 @@
 vim.cmd [[cnoreabbrev bash botright terminal]]
 -- vash is a short-term for 'vertical bash'
 vim.cmd [[cnoreabbrev vash vertical botright terminal]]
+
+-- Create commands to toggle formatting
+vim.api.nvim_create_user_command("FormatDisable", function(args)
+    if args.bang then
+        -- :FormatDisable! disables just for this buffer
+        vim.b.disable_autoformat = true
+    else
+        -- :FormatDisable disables globally
+        vim.g.disable_autoformat = true
+    end
+end, {
+    desc = "Disable autoformat-on-save",
+    bang = true,
+})
+
+vim.api.nvim_create_user_command("FormatEnable", function()
+    vim.b.disable_autoformat = false
+    vim.g.disable_autoformat = false
+end, {
+    desc = "Re-enable autoformat-on-save",
+})
--- link/.config/nvim/lua/plugins/formatter.lua
+++ link/.config/nvim/lua/plugins/formatter.lua
@@ -4,6 +4,13 @@ return {
     cmd = { "ConformInfo" },
     config = function()
         require("conform").setup({
+            format_on_save = function(bufnr)
+                -- Disable with a global or buffer-local variable
+                if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+                    return
+                end
+                return { timeout_ms = 500, lsp_fallback = true }
+            end,
             formatters_by_ft = {
                 -- Fortran
                 fortran = { "fprettify" },
